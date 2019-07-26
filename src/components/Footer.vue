5965b7b5eca7fd747d643d26485ce1b593e96836
Ygor Tavela <ygortavela@gmail.com>
2019-07-25 21:28:07 -0300

    finish partner and footer components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,47 @@
 <template>
-  <section id="footer"></section>
+  <section id="footer">
+    <div>
+      <p>
+        <strong>Edições anteriores</strong><br />
+        <a href="http://www.ime.usp.br/~encontrobcc/2010/">II 2010</a> &middot;
+        <a href="http://www.ime.usp.br/~encontrobcc/2011/">III 2011</a> &middot;
+        <a href="http://www.ime.usp.br/~encontrobcc/2012/">IV 2012</a> &middot;
+        <a href="http://www.ime.usp.br/~encontrobcc/2013/">V 2013</a> &middot;
+        <a href="http://bcc.ime.usp.br/40anos/">VI 2014</a> &middot;
+        <a href="http://bcc.ime.usp.br/encontro/2015">VII 2015</a> &middot;
+        <a href="http://bcc.ime.usp.br/encontro/2016/#/">VIII 2016</a> &middot;
+        <a href="https://bcc.ime.usp.br/encontro/2017">IV 2017</a>
+      </p>
+      <p>
+        <a href="https://www.facebook.com/semanacomputacaoimeusp/"
+          >Página no Facebook</a
+        >
+      </p>
+      <p><strong>Semana da Computação &copy; 2018</strong><br /></p>
+      <p>
+        Ícones desenvolvidos pela
+        <a href="http://www.freepik.com/">Freepik</a> de
+        <a href="https://www.flaticon.com">www.flaticon.com</a>.
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style>
+#footer {
+  @apply bg-gray-200 text-black text-base pt-8 text-center border-t-4;
+  text-shadow: none;
+}
+
+a {
+  @apply text-blue-600 underline px-2;
+}
+
+p {
+  @apply mb-6;
+}
+</style>
